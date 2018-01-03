06d8005958f80dad4cd964e607e0d2d04d112704
Andy Bell <me@andy-bell.co.uk>
2018-01-03 21:02:39 +0000

    Add some docs

--- assets/js/modules/validation.js
+++ assets/js/modules/validation.js
@@ -1,19 +1,32 @@
 export default class Validation {
 
+    /**
+     * Load up an instance of Validation
+     * @param {HTMLFormElement} baseForm 
+     */
     constructor(baseForm) {
         this.baseForm = baseForm;
+
+        // Load child input elements
         this.inputElems = [...baseForm.querySelectorAll('input, textarea, select')];
     }
 
+    /**
+     * Public init method
+     */
     init() {
         let self = this;
 
         self.bind();
     }
 
+    /**
+     * Bind events to input elements
+     */
     bind() {
         let self = this;
 
+        // Add an invalid listener that 
         self.inputElems.map(item => {
             item.addEventListener('invalid', evt => {
                 self.process(item, 'invalid');
@@ -22,6 +35,11 @@ export default class Validation {
         });
     }
 
+    /**
+     * Toggle the visual state of an item based on the based state key
+     * @param {HTMLElement} item 
+     * @param {String} state 
+     */
     process(item, state = 'invalid') {
         let self = this;
 
@@ -35,16 +53,23 @@ export default class Validation {
         } 
     }
 
+    /**
+     * Check an item's siblings validty state
+     * @param {HTMLElement} item 
+     */
     checkSiblings(item) {
         let self = this;
+
+        // Find siblings that aren't this item and that are required
         let inputElems = self.inputElems.filter(elem => elem != item && elem.hasAttribute('required'));
         
         if(inputElems.length) {
 
             inputElems.map(item => {
 
-                if(item.validity.valid && item.classList.contains('is-error')) {
-                    item.classList.remove('is-error');
+                // If an item is valid, run the processor 
+                if(item.validity.valid) {
+                    self.process(item, 'valid');
                 } 
             });
         }
