bd593fb9308a6e9250f9c4a4412d7daad2875333
Andy Bell <me@andy-bell.co.uk>
2018-04-07 20:36:01 +0100

    Remove custom validity JS

--- assets/js/boilerform.js
+++ assets/js/boilerform.js
@@ -1,13 +1,13 @@
 import Validation from './modules/validation';
 
 // Import Sass so that webpack picks it up
-import BoilerFormCSS from '../scss/boilerform.scss';
+require('../scss/boilerform.scss');
 
 (function() {
 
     // Look for child and root forms 
-    const boilerforms = [...document.querySelector('.boilerform form, form.boilerform')];
-
+    const boilerforms = [...document.querySelectorAll('.boilerform form, form.boilerform')];
+    
     if(boilerforms.length) {
 
         // Add a validator to each form instance
